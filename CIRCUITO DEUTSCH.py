2159918b8f74ccb883b1dc4d3d3c9bc0586f1b2b
murcia0421 <112002572+murcia0421@users.noreply.github.com>
2022-11-27 12:57:23 -0500

    Update CIRCUITO DEUTSCH.py

--- CIRCUITO DEUTSCH.py
+++ CIRCUITO DEUTSCH.py
@@ -3,3 +3,137 @@ from qiskit import QuantumCircuit, transpile
 from qiskit import Aer
 from qiskit.visualization import plot_histogram
 import matplotlib.pyplot as 
+
+
+print("Juan Daniel Murcia")
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+# Create a Quantum Circuit acting on the q register
+circuit = QuantumCircuit(2, 2)
+#circuit.x(1) [01]
+#circuit.x(0) [10]
+#->
+#circuit.x(0). circuit.x [11]
+circuit.barrier(0,1)
+## (0,1) -> (1,0)
+circuit.x(0)
+circuit.cx(0,1)
+circuit.x(0)
+circuit.barrier(0,1)
+# Map the quantum measurement to the classical bits
+circuit.measure([0,1], [1,0])
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+# Grab results from the job
+result = job.result()
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+# Draw the circuit
+print(circuit)
+# Plot a histogram
+plot_histogram(counts)
+plt.show()
+
+
+
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+# Create a Quantum Circuit acting on the q register
+circuit = QuantumCircuit(2, 2)
+#circuit.x(1) [01]
+#circuit.x(0) [10]
+#->
+#circuit.x(0) circuit.x [11]
+circuit.barrier(0,1)
+## (0,1) -> (1,0)
+circuit.i(0)
+circuit.i(1)
+#circuit.x(0)
+circuit.barrier(0,1)
+# Map the quantum measurement to the classical bits
+circuit.measure([1,0], [0,1])
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+# Grab results from the job
+result = job.result()
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+# Draw the circuit
+print(circuit)
+# Plot a histogram
+plot_histogram(counts)
+plt.show()
+
+
+
+# Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+# Create a Quantum Circuit acting on the q register
+circuit = QuantumCircuit(2, 2)
+#circuit.x(1) [01]
+#circuit.x(0) [10]
+#->
+#circuit.x(0) circuit.x [11]
+circuit.barrier(0,1)
+## (0,1) -> (0,|)
+circuit.cx(0,1)
+#circuit.i(1)
+#circuit.x(0)
+circuit.barrier(0,1)
+# Map the quantum measurement to the classical bits
+circuit.measure([1,0], [0,1])
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+# Grab results from the job
+result = job.result()
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+# Draw the circuit
+print(circuit)
+# Plot a histogram
+plot_histogram(counts)
+plt.show()
+
+
+
+ Use Aer's qasm_simulator
+simulator = Aer.get_backend('qasm_simulator')
+# Create a Quantum Circuit acting on the q register
+circuit = QuantumCircuit(2, 2)
+#circuit.x(1) [01]
+#circuit.x(0) [10]
+#->
+#circuit.x(0) cicuit.x [11]
+circuit.barrier(0,1)
+## (0,1) -> (1,1)
+circuit.x(1)
+circuit.barrier(0,1)
+# Map the quantum measurement to the classical bits
+circuit.measure([1,0], [0,1])
+# compile the circuit down to low-level QASM instructions
+# supported by the backend (not needed for simple circuits)
+compiled_circuit = transpile(circuit, simulator)
+# Execute the circuit on the qasm simulator
+job = simulator.run(compiled_circuit, shots=1000)
+# Grab results from the job
+result = job.result()
+# Returns counts
+counts = result.get_counts(circuit)
+print("\nTotal count for 00 and 11 are:",counts)
+# Draw the circuit
+print(circuit)
+# Plot a histogram
+plot_histogram(counts)
+plt.show()
